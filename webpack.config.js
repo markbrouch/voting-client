9f5425aeb2cc8e8d244b040418aceb49a336fa75
Mark Brouch <mark.brouch@gmail.com>
2015-12-07 16:36:48 -0600

    Set up webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,23 @@ module.exports = {
   entry: [
     './src/index.js'
   ],
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'babel'
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
   output: {
     path: __dirname + '/dist',
     publicPath: '/',
     filename: 'bundle.js'
   },
   devServer: {
-    contentBase: './dist'
+    contentBase: './dist',
+    port: process.env.PORT || 3000
   }
 };
